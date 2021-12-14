65e58d82cb012c39e9b9f1a2aef9d422fad07cda
ZubirForest <andrew@andrewmc.ca>
2021-12-13 20:53:22 -0400

    packing-list with while loops

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,20 @@
+// For loops over arrays (review)
+
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+/*
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+*/
+
+// Can use while loops instead
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
